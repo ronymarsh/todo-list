bf946c1ed37d01982aeda71d801d43e8947e9189
ronymarsh <rony.marsh6@gmail.com>
2021-08-30 13:45:37 +0300

    setup webpack & babel in client

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
+// Required External Modules
 const express = require('express');
 
+// App Variables
 const app = express();
+const PORT = process.env.PORT || 5000;
 
+// App Configuration
+
+// Routes Definitions
+app.get('/', (req, res) => {
+  res.status(200).send('Todo List Server!');
+});
+
+// Server Activation
 app.listen(5000, () => {
   console.log('Listening on port 5000!');
 });
