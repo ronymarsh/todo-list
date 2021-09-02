4c660b4fbd1991bd0eec8593b44cb64f6c3f3878
ronymarsh <rony.marsh6@gmail.com>
2021-09-02 18:31:36 +0300

    client-server folders restructure, basic webpack setup in client

---
+++ client/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const { webpack } = require('webpack');
+
+// const plugins = [new CleanWebpackPlugin(), new MiniCssExtractPlugin()];
+
+module.exports = {
+  mode: 'development',
+
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          // without additional settings, this will reference .babelrc
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devtool: 'source-map',
+
+  devServer: {
+    port: 3000,
+    hot: true,
+  },
+};
