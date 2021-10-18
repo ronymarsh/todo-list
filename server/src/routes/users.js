878fdb59ce69d0bc52b1c51934f92c179223a65d
ronymarsh <rony.marsh6@gmail.com>
2021-10-18 16:04:59 +0300

    current user route  fix

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -10,7 +10,6 @@ const UsersRouter = Router();
 
 UsersRouter.get('/currentuser', (req, res, next) => {
   passport.authenticate('bearer', function (err, user) {
-    if (!user) return res.status(441).send({ currentUser: null });
     if (err) return res.status(440).send({ currentUser: null });
     req.logIn(user, { session: false }, function (err) {
       console.log('CURRENT USER ROUTE: ', user);
@@ -20,67 +19,57 @@ UsersRouter.get('/currentuser', (req, res, next) => {
   })(req, res, next);
 });
 
-UsersRouter.post(
-  '/signin',
-  [
-    body('email').isEmail().withMessage('Email not valid'),
-    body('password')
-      .trim()
-      .notEmpty()
-      .withMessage('You must supply a password'),
-  ],
-  async (req, res) => {
-    const { email, password } = req.body;
+UsersRouter.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
 
-    const existingUser = await User.findOne({ email });
-    if (!existingUser)
-      return res
-        .status(400)
-        .send([{ msg: 'Invalid Cradentials', param: 'signin' }]);
+  const existingUser = await User.findOne({ email });
+  if (!existingUser)
+    return res
+      .status(400)
+      .send([{ msg: 'Invalid Cradentials', param: 'signin' }]);
 
-    const passwordsMatch = await Password.compare(
-      existingUser.password,
-      password
-    );
+  const passwordsMatch = await Password.compare(
+    existingUser.password,
+    password
+  );
 
-    if (!passwordsMatch)
-      return res
-        .status(400)
-        .send([{ msg: 'Invalid Cradentials', param: 'signin' }]);
+  if (!passwordsMatch)
+    return res
+      .status(400)
+      .send([{ msg: 'Invalid Cradentials', param: 'signin' }]);
 
-    // Generate an access token valid for 1 minute
-    const accessToken = jwt.sign(
-      {
-        id: existingUser.id,
-      },
-      process.env.JWT_ACCESS_KEY,
-      {
-        expiresIn: '60s',
-      }
-    );
+  // Generate an access token valid for 1 minute
+  const accessToken = jwt.sign(
+    {
+      id: existingUser.id,
+    },
+    process.env.JWT_ACCESS_KEY,
+    {
+      expiresIn: '60s',
+    }
+  );
 
-    // Generate a refresh token valid for 30 days or until logout
-    const refreshToken = jwt.sign(
-      {
-        id: existingUser.id,
-      },
-      process.env.JWT_REFRESH_KEY,
-      {
-        expiresIn: '30 days',
-      }
-    );
+  // Generate a refresh token valid for 30 days or until logout
+  const refreshToken = jwt.sign(
+    {
+      id: existingUser.id,
+    },
+    process.env.JWT_REFRESH_KEY,
+    {
+      expiresIn: '30 days',
+    }
+  );
 
-    // save the refreshToken in db for token tracking
-    const user = existingUser.id;
-    const value = refreshToken;
-    // if there is a token in db with same user, delete it before saving
-    await RefreshToken.deleteOne({ user });
-    // create object
-    const refreshTokenObj = new RefreshToken({ user, value });
-    await refreshTokenObj.save().catch((err) => console.log(err));
-    return res.status(200).send({ accessToken, refreshToken });
-  }
-);
+  // save the refreshToken in db for token tracking
+  const user = existingUser.id;
+  const value = refreshToken;
+  // if there is a token in db with same user, delete it before saving
+  await RefreshToken.deleteOne({ user });
+  // create object
+  const refreshTokenObj = new RefreshToken({ user, value });
+  await refreshTokenObj.save().catch((err) => console.log(err));
+  return res.status(200).send({ accessToken, refreshToken });
+});
 
 UsersRouter.post(
   '/signup',
@@ -205,13 +194,5 @@ UsersRouter.post('/signout', async (req, res) => {
   await RefreshToken.deleteOne({ user });
   return res.status(200).send({});
 });
-
-UsersRouter.get(
-  '/test',
-  passport.authenticate('bearer', { session: false }),
-  (req, res) => {
-    res.send({});
-  }
-);
 
 module.exports = UsersRouter;
